2d2787b81bbc346b9b9ab896ccf6cb672054bd6e
Gustavo Felix <gusfelix2015@gmail.com>
2020-11-28 14:47:24 -0300

    Adding template ingine in recipes

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ module.exports = [
     image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/burger.png",
     title: "Triplo bacon burger",
     author: "Jorge Relato",
+    id: "burger",
     ingredients: [
       "3 kg de carne moída (escolha uma carne magra e macia)",
       "300 g de bacon moído",
@@ -27,6 +28,7 @@ module.exports = [
     image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/pizza.png",
     title: "Pizza 4 estações",
     author: "Fabiana Melo",
+    id: "pizza",
     ingredients: [
       "1 xícara (chá) de leite",
       "1 ovo",
@@ -55,6 +57,7 @@ module.exports = [
     image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/asinha.png",
     title: "Asinhas de frango ao barbecue",
     author: "Vania Steroski",
+    id: "asinhas",
     ingredients: [
       "12 encontros de asinha de galinha, temperados a gosto",
       "2 colheres de sopa de farinha de trigo",
@@ -72,6 +75,7 @@ module.exports = [
     image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/lasanha.png",
     title: "Lasanha mac n' cheese",
     author: "Juliano Vieira",
+    id: "lasanha",
     ingredients: [
       "massa pronta de lasanha",
       "400 g de presunto",
@@ -92,6 +96,7 @@ module.exports = [
     image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/macarrao.png",
     title: "Espaguete ao alho",
     author: "Júlia Kinoto",
+    id: "espaguete",
     ingredients: [
       "1 pacote de macarrão 500 g (tipo do macarrão a gosto)",
       "1 saquinho de alho granulado",
@@ -117,6 +122,7 @@ module.exports = [
     image: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/doce.png",
     title: "Docinhos pão-do-céu",
     author: "Ricardo Golvea",
+    id: "doce",
     ingredients: [
       "1 kg de batata - doce",
       "100 g de manteiga",
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ server.get("/about", (req, res) => {
 });
 
 server.get("/recipes", (req, res) => {
-  return res.render("recipes");
+  return res.render("recipes", { items: recipes });
 });
 
 server.listen(5000, () => {
