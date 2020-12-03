780f7bcc64d35e6f4c12589018a94d3ba034742c
Gustavo Felix <gusfelix2015@gmail.com>
2020-12-03 12:31:19 -0300

    Create script show and hide

--- public/script.js
+++ public/script.js
@@ -1,7 +1,21 @@
 const cards = document.querySelectorAll(".card");
+const showHides = document.getElementsByClassName("topic");
 for (let card of cards) {
   card.addEventListener("click", function () {
     const recipeId = card.getAttribute("Id");
     window.location.href = `/recipes/${recipeId}`;
+  });
+}
+
+for (let showHide of showHides) {
+  const button = showHide.querySelector("button");
+  button.addEventListener("click", () => {
+    if (button.innerHTML == "ESCONDER") {
+      showHide.querySelector(".topic-content").classList.add("hide");
+      button.innerHTML = "MOSTRAR";
+    } else {
+      showHide.querySelector(".topic-content").classList.remove("hide");
+      button.innerHTML = "ESCONDER";
+    }
   });
 }
