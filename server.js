b4f15c0d5e4ba6d979a237df474d98d892dcc141
Gustavo Felix <gusfelix2015@gmail.com>
2021-01-28 22:16:22 -0300

    Add method override, routes, get routes and dataJson

--- server.js
+++ server.js
@@ -1,38 +1,22 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const routes = require("./routes");
+const methodOverride = require("method-override");
+const recipes = require("./data");
 
 const server = express();
-const recipes = require("./data");
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static("public"));
+server.use(methodOverride("_method"));
+server.use(routes);
 
 server.set("view engine", "njk");
 
-server.use(express.static("public"));
-
 nunjucks.configure("views", {
   express: server,
-  autoescape: false,
+  autoscape: false,
   noCache: true,
-});
-
-server.get("/", (req, res) => {
-  return res.render("home", { items: recipes });
-});
-
-server.get("/about", (req, res) => {
-  return res.render("about");
-});
-
-server.get("/recipes", (req, res) => {
-  return res.render("recipes", { items: recipes });
-});
-
-server.get("/recipe/:index", function (req, res) {
-  const recipeIndex = req.params.index;
-  const recipe = recipes.find((recipe) => String(recipe.id) === recipeIndex);
-  if (!recipe) {
-    return res.send("Recipe not found!");
-  }
-  return res.render("recipe", { item: recipe });
 });
 
 server.listen(5000, () => {
