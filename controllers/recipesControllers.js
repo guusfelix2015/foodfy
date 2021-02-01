24c90aa742e2142c8a2501adae81f20cc4b65922
Gustavo Felix <gusfelix2015@gmail.com>
2021-01-31 22:40:16 -0300

    Create layout recipe

---
+++ controllers/recipesControllers.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const data = require("../data.json");
+
+exports.index = (req, res) => {
+  return res.render("admin/index");
+};
+
+exports.create = (req, res) => {
+  return res.render("admin/create");
+};
+
+exports.post = (req, res) => {
+  const keys = Object.keys(req.body);
+
+  for (key of keys) {
+    if (req.body[key] == "") return res.send("Preencha todos os campos");
+  }
+
+  let id = 1;
+  const lastRecipe = data.recipes[data.recipes.length + 1];
+
+  if (lastRecipe) {
+    id = lastRecipe + 1;
+  }
+
+  data.recipes.push({
+    id,
+    ...req.body,
+  });
+  
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("ERROR");
+    return res.redirect(`/admin/recipes/${id}`);
+  });
+};
--- routes.js
+++ routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const routes = express.Router();
 
+const recipes = require("./controllers/recipesControllers");
 const site = require("./controllers/siteController");
 
+// SITE
 routes.get("/", site.home);
 routes.get("/about", site.about);
 routes.get("/recipes", site.recipes);
 routes.get("/recipe/:id", site.recipe);
+
+//ADMIN
+routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+
+routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+
 
 module.exports = routes;
