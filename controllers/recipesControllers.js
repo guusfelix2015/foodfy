8f63e753bde668e9ed9d08f46e9c1894c3234f67
Gustavo Felix <gusfelix2015@gmail.com>
2021-02-01 22:59:34 -0300

    Get recipe params

--- controllers/recipesControllers.js
+++ controllers/recipesControllers.js
@@ -9,6 +9,22 @@ exports.create = (req, res) => {
   return res.render("admin/create");
 };
 
+exports.show = (req, res) => {
+  const { id } = req.params;
+
+  const foundRecipe = data.recipes.find((recipe) => {
+    return (recipe.id = id);
+  });
+
+  if (!foundRecipe) return res.send("Receita não econtrada");
+
+  const recipe = {
+    ...foundRecipe,
+  };
+
+  return res.render("admin/show", { recipe });
+};
+
 exports.post = (req, res) => {
   const keys = Object.keys(req.body);
 
@@ -27,7 +43,7 @@ exports.post = (req, res) => {
     id,
     ...req.body,
   });
-  
+
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send("ERROR");
     return res.redirect(`/admin/recipes/${id}`);
