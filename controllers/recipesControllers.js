8e81e042f5d0bfb9756cf3e0e43dc54dfca1b8ab
Gustavo Felix <gusfelix2015@gmail.com>
2021-02-04 23:34:43 -0300

    Create edit and put recipes

--- controllers/recipesControllers.js
+++ controllers/recipesControllers.js
@@ -2,18 +2,14 @@ const fs = require("fs");
 const data = require("../data.json");
 
 exports.index = (req, res) => {
-  return res.render("admin/index");
-};
-
-exports.create = (req, res) => {
-  return res.render("admin/create");
+  return res.render("admin/index", { recipes: data.recipes });
 };
 
 exports.show = (req, res) => {
   const { id } = req.params;
 
   const foundRecipe = data.recipes.find((recipe) => {
-    return (recipe.id = id);
+    return id == recipe.id;
   });
 
   if (!foundRecipe) return res.send("Receita não econtrada");
@@ -23,6 +19,10 @@ exports.show = (req, res) => {
   };
 
   return res.render("admin/show", { recipe });
+};
+
+exports.create = (req, res) => {
+  return res.render("admin/create");
 };
 
 exports.post = (req, res) => {
@@ -47,5 +47,50 @@ exports.post = (req, res) => {
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send("ERROR");
     return res.redirect(`/admin/recipes/${id}`);
+  });
+};
+
+exports.edit = (req, res) => {
+  const { id } = req.params;
+
+  const foundRecipe = data.recipes.find((recipe) => {
+    return id == recipe.id;
+  });
+
+  if (!foundRecipe) {
+    return res.send("Receita não encontrada");
+  }
+
+  const recipe = {
+    ...foundRecipe,
+  };
+
+  return res.render("admin/edit", { recipe });
+};
+
+exports.put = (req, res) => {
+  const { id } = req.body;
+  let index = 0;
+
+  const foundRecipe = data.recipes.find((recipe, foundIndex) => {
+    if (id == recipe.id) {
+      index = foundIndex;
+      return true;
+    } 
+  });
+
+  if (!foundRecipe) return res.send("Receita não encontrada");
+
+  const recipe = {
+    ...foundRecipe,
+    ...req.body,
+    id: Number(req.body.id),
+  };
+
+  data.recipes[index] = recipe;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("Error");
+    return res.redirect(`recipes/${id}`);
   });
 };
