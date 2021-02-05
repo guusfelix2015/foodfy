904b814aad798209de1df0664f7720735368ab6f
Gustavo Felix <gusfelix2015@gmail.com>
2021-02-04 23:35:51 -0300

    Create routes edit and put recipes

--- routes.js
+++ routes.js
@@ -14,6 +14,9 @@ routes.get("/recipe/:id", site.recipe);
 routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
 routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
 routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id/edit", recipes.edit);
+
 routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+routes.put("/admin/recipes", recipes.put); // Editar uma receita
 
 module.exports = routes;
